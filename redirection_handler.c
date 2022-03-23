2b44e8cbe78e65bd6698d0b834e8885cdefec047
iidkhebb <iidkhebb@student.1337.ma>
2022-03-23 22:56:08 +0100

    added heredco on parsing side

--- redirection_handler.c
+++ redirection_handler.c
@@ -9,7 +9,7 @@ int heredoc(t_command *command, t_envlist *lst)
 	while (1)
 	{
 		prompt = readline("\x1b[34mheredoc > \x1b[37m");
-		if (!ft_strcmp(command->files->file, prompt))
+		if (!prompt || !ft_strcmp(command->files->file, prompt))
 			return (free(prompt), close(fd[1]) ,fd[0]);
 		ft_putstr_fd(__get_env(prompt, lst), fd[1]);
 		ft_putstr_fd("\n", fd[1]);
@@ -43,7 +43,6 @@ void left_redirection(t_command *command, int file_out)
 	}
 	else if (!ft_strcmp(command->redirection, "<<"))
 	{
-		// file_out = heredoc(command, lst);
 		dup2(command->heredoc, 0);
 		close(command->heredoc);
 	}
