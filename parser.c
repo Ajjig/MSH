b809449ffd5629216c842298e7bee7ce51a7dea1
Ajjig <ajjigsimo@gmail.com>
2022-03-25 19:34:02 +0100

    fix SIGSEGV in echo with -n option

--- parser.c
+++ parser.c
@@ -2,12 +2,12 @@
 
 void	parser(t_command *command, char **buff,  int *i, int *ai)
 {
-	if (buff[*i] && *i == 1 && !ft_strcmp(buff[*i], "-n") && !ft_strcmp(_ECHO, command -> program))
+	if (buff[*i] && *i == 1 && !ft_strcmp(buff[*i], "-n")
+		&& !ft_strcmp(_ECHO, command -> program))
 	{
 		command -> options = ft_strdup(buff[*i]);
-		*i += 1;
 	}
-	if (buff[*i] && *i > 0 && !ft_strchr(REDIRECTIONS, buff[*i][0]))
+	else if (buff[*i] && *i > 0 && !ft_strchr(REDIRECTIONS, buff[*i][0]))
 	{
 		command -> args[*ai] = ft_strdup(buff[*i]);
 		*ai += 1;
